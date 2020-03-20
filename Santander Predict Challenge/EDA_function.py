48b21c758d460789b1b080056a9cebe0cfb3dec1
henryHTH <thhu@bu.edu>
2020-03-20 16:02:34 -0400

    Update EDA_function.py

--- Santander Predict Challenge/EDA_function.py
+++ Santander Predict Challenge/EDA_function.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import spearmanr
-def EDA(file_name):
-    data = pd.read_csv(file_name)
+def EDA(file_name,data_fraction=None):
+    print('version 1')
+    if data_fraction is None:
+        data = pd.read_csv(file_name)
+    else:
+        data = pd.read_csv(file_name)
+        rows = int(data.shape[0]*data_fraction)
+        data = data.sample(n=rows, random_state=1)
     target = data['target']
     df = data.iloc[:,2:]
     zero_rate = []
@@ -23,4 +29,9 @@ def EDA(file_name):
         rank_corr_p.append(p)
     df_info = {'type':df.dtypes,'var':col_var,'unique_number':col_unique_num,'corr':target_cor,'zero_rate':zero_rate,'spearman_corr':rank_corr,'spearman_corr_p':rank_corr_p}
     df_info = pd.DataFrame(df_info).T
-    return data,df_info
+    target = data['target']
+    data = data.drop('target',axis = 1)
+    df = data.set_index('ID')
+    all_zero_l = df_info.columns[np.where(df_info.loc['zero_rate']>=.95)]
+    df = df.drop(all_zero_l,axis=1)
+    return df,target,df_info
